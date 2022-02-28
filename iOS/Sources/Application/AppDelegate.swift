a262a8f341d4d4c1f065985b083fa386ae52eac4
baegteun <baegteun@gmail.com>
2022-03-01 00:10:22 +0900

    :sparkles: :: Firebase Messaging Setting

--- iOS/Sources/Application/AppDelegate.swift
+++ iOS/Sources/Application/AppDelegate.swift
@@ -1,12 +1,28 @@
 import UIKit
 import Swinject
+import Service
+import FirebaseMessaging
+import FirebaseCore
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     static let container = Container()
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        setFCM(application)
+        AppDelegate.container.registerDependencies()
+        AppDelegate.container.registerServiceDependencies()
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = true
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         
         return true
+    }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
     }
 
     // MARK: UISceneSession Lifecycle
@@ -16,9 +32,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        
     }
 
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+        if #available(iOS 14.0, *) {
+            return [.badge, .sound, .list, .banner]
+        } else {
+            return [.alert, .badge, .sound]
+        }
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+        return
+    }
+}
+
+private extension AppDelegate {
+    func setFCM(_ application: UIApplication){
+        UNUserNotificationCenter.current().delegate = self
+        let authOption: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: authOption, completionHandler: { _,_ in } )
+        application.registerForRemoteNotifications()
+    }
+}
