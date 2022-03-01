b9dce4f53c567ee6a4add6b3cceb5cfe6b2c36ef
baegteun <baegteun@gmail.com>
2022-03-01 20:15:04 +0900

    :bento: Asset Fetching

--- iOS/Derived/Sources/Assets+GCMS.swift
+++ iOS/Derived/Sources/Assets+GCMS.swift
@@ -17,10 +17,12 @@
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 public enum GCMSAsset {
-  public enum Assets {
-    public static let accentColor = GCMSColors(name: "AccentColor")
+  public enum Colors {
+    public static let gcmsBackgroundColor = GCMSColors(name: "GCMS_BackgroundColor")
+    public static let gcmsMainColor = GCMSColors(name: "GCMS_MainColor")
   }
   public enum Images {
+    public static let accentColor = GCMSColors(name: "AccentColor")
   }
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
