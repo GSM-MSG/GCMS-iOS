501060a1a76e0448aa8a76e37035fb2339b301ad
baegteun <baegteun@gmail.com>
2022-03-01 12:32:18 +0900

    :sparkles: :: HeaderLabel configuration

--- iOS/Sources/Presentation/Scene/OnBoarding/VC/OnBoardingVC.swift
+++ iOS/Sources/Presentation/Scene/OnBoarding/VC/OnBoardingVC.swift
@@ -1,9 +1,14 @@
 import UIKit
 import GoogleSignIn
+import Then
 
 final class OnBoardingVC: BaseVC<OnBoardingReactor> {
     // MARK: - Properties
-    private let headerLabel = UILabel()
+    private let headerLabel = UILabel().then {
+        $0.text = "GSM동아리\n신청앱"
+        $0.textColor = .white
+        $0.numberOfLines = 0
+    }
     private let nameLabel = UILabel()
     private let logoImageView = UIImageView()
     private let googleSigninButton = GIDSignInButton()
