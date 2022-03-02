b105757ec584ef53263457f252f07d888785471c
baegteun <baegteun@gmail.com>
2022-03-02 15:54:04 +0900

    :sparkles: :: ClubList Entity

---
+++ Service/Sources/Domain/Entities/ClubList.swift
@@ -0,0 +1,10 @@
+
+public struct ClubList {
+    public let bannerUrl: String
+    public let title: String
+    
+    public init(bannerUrl: String, title: String) {
+        self.bannerUrl = bannerUrl
+        self.title = title
+    }
+}
