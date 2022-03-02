13fc3fccef9bca34ce73633f5beacc8a557d1649
baegteun <baegteun@gmail.com>
2022-03-03 05:46:35 +0900

    :sparkles: :: UINavigationIem extension
    configuration backbutton

---
+++ iOS/Sources/Presentation/Utils/Extensions/UIKit/UINavigation/UINavigationExt.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+extension UINavigationItem {
+    func configTitle(
+        title: String,
+        font: UIFont = UIFont(font: GCMSFontFamily.Inter.semiBold, size: 16) ?? .init()
+    ) {
+        let lb = UILabel()
+        lb.font = font
+        lb.textColor = .white
+        lb.text = title
+        self.titleView = lb
+    }
+    func configBack(){
+        let back = UIBarButtonItem(title: "돌아가기", style: .plain, target: nil, action: nil)
+        back.tintColor = GCMSAsset.Colors.gcmsGray1.color
+        self.backBarButtonItem = back
+    }
+}
+
+extension UINavigationBar {
+    func setClear() {
+        self.setBackgroundImage(UIImage(), for: .default)
+        self.shadowImage = UIImage()
+        self.backgroundColor = .clear
+    }
+}
