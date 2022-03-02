60697a7a1402da8d448a2462a95a5bbba58d013d
baegteun <baegteun@gmail.com>
2022-03-02 15:04:00 +0900

    :sparkles: :: BaseTableViewCell

---
+++ iOS/Sources/Presentation/Base/BaseTableViewCell.swift
@@ -0,0 +1,30 @@
+import UIKit
+import RxSwift
+
+class BaseTableViewCell<T>: UITableViewCell{
+    let bound = UIScreen.main.bounds
+    lazy var disposeBag: DisposeBag = .init()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addView()
+        setLayout()
+        configureCell()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setLayoutSubviews()
+    }
+    func addView(){}
+    func setLayout(){}
+    func setLayoutSubviews(){}
+    func configureCell(){}
+    var model: T? {
+        didSet { if let model = model { bind(model) } }
+    }
+    func bind(_ model: T){}
+}
