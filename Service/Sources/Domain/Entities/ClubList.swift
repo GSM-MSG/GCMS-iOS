17833f4c6c407ff10f7ca0b8d95eb3b91d0e6090
baegteun <baegteun@gmail.com>
2022-03-02 16:34:21 +0900

    :sparkles: :: ClubLIst

--- Service/Sources/Domain/Entities/ClubList.swift
+++ Service/Sources/Domain/Entities/ClubList.swift
@@ -2,9 +2,15 @@
 public struct ClubList {
     public let bannerUrl: String
     public let title: String
+    public let type: ClubType
     
-    public init(bannerUrl: String, title: String) {
+    public init(
+        bannerUrl: String,
+        title: String,
+        type: ClubType
+    ) {
         self.bannerUrl = bannerUrl
         self.title = title
+        self.type = type
     }
 }
--- iOS/Sources/Presentation/Scene/Main/Home/VC/HomeVC.swift
+++ iOS/Sources/Presentation/Scene/Main/Home/VC/HomeVC.swift
@@ -4,10 +4,17 @@ import PinLayout
 import RxSwift
 import RxCocoa
 import RxDataSources
+import Reusable
 
 final class HomeVC: BaseVC<HomeReactor> {
     // MARK: - Properties
     private let clubTypeSegmentedControl = ClubTypeSegmentedControl(titles: ["전공", "사설", "자율"])
+    private let clubListCollectionView = UICollectionView(frame: .zero, collectionViewLayout: .init()).then {
+        let layout = UICollectionViewFlowLayout()
+        
+        $0.register(cellType: ClubListCell.self)
+        $0.collectionViewLayout = layout
+    }
     
     // MARK: - UI
     override func addView() {
@@ -22,11 +29,13 @@ final class HomeVC: BaseVC<HomeReactor> {
     
     // MARK: - Reactor
     override func bindAction(reactor: HomeReactor) {
-        
+        self.rx.viewDidLoad
+            .map { Reactor.Action.viewDidLoad }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
     }
     override func bindState(reactor: HomeReactor) {
         let sharedState = reactor.state.share(replay: 1).observe(on: MainScheduler.asyncInstance)
-        
         
     }
 }
