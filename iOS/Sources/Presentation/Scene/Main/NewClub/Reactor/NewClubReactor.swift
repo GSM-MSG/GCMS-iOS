eae189fffb593e4a32c42e9bbcd96256e7a852e2
baegteun <baegteun@gmail.com>
2022-03-03 23:37:32 +0900

    :sparkles: :: NewClub Setup

--- iOS/Sources/Presentation/Scene/Main/NewClub/Reactor/NewClubReactor.swift
+++ iOS/Sources/Presentation/Scene/Main/NewClub/Reactor/NewClubReactor.swift
@@ -1,9 +1,57 @@
-//
-//  NewClubReactor.swift
-//  GCMS
-//
-//  Created by 최형우 on 2022/03/03.
-//  Copyright © 2022 baegteun. All rights reserved.
-//
+import ReactorKit
+import RxFlow
+import RxSwift
+import RxRelay
 
-import Foundation
+final class NewClubReactor: Reactor, Stepper {
+    // MARK: - Properties
+    var steps: PublishRelay<Step> = .init()
+    
+    private let disposeBag: DisposeBag = .init()
+    
+    // MARK: - Reactor
+    enum Action {
+        
+    }
+    enum Mutation {
+        
+    }
+    struct State {
+        
+    }
+    let initialState: State
+    
+    // MARK: - Init
+    init() {
+        initialState = State()
+    }
+    
+}
+
+// MARK: - Mutate
+extension NewClubReactor {
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+            
+        }
+        return .empty()
+    }
+}
+
+// MARK: - Reduce
+extension NewClubReactor {
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        
+        switch mutation {
+            
+        }
+        
+        return newState
+    }
+}
+
+// MARK: - Method
+private extension NewClubReactor {
+    
+}
--- iOS/Sources/Presentation/Scene/Main/NewClub/VC/NewClubVC.swift
+++ iOS/Sources/Presentation/Scene/Main/NewClub/VC/NewClubVC.swift
@@ -1,9 +1,6 @@
-//
-//  NewClubVC.swift
-//  GCMS
-//
-//  Created by 최형우 on 2022/03/03.
-//  Copyright © 2022 baegteun. All rights reserved.
-//
+import UIKit
 
-import Foundation
+final class NewClubVC: BaseVC<NewClubReactor> {
+    // MARK: - Properties
+    
+}
