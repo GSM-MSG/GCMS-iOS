35883f8d4c96fb2c94bc0c69380eef620726f269
baegteun <baegteun@gmail.com>
2022-03-05 15:42:06 +0900

    :sparkles: :: ImageData Append

--- iOS/Sources/Presentation/Scene/Main/NewClub/Reactor/NewClubReactor.swift
+++ iOS/Sources/Presentation/Scene/Main/NewClub/Reactor/NewClubReactor.swift
@@ -13,18 +13,28 @@ final class NewClubReactor: Reactor, Stepper {
     // MARK: - Reactor
     enum Action {
         case imageDidSelect(Data)
+        case bannerDidTap
+        case activityAppendButtonDidTap
+        case activityDeleteDidTap(Int)
     }
     enum Mutation {
         case setImageData(Data)
+        case setIsBanner(Bool)
+        case removeImageData(Int)
     }
     struct State {
+        var isBanner: Bool
         var imageData: Data?
+        var activitiesData: [Data]
     }
     let initialState: State
     
     // MARK: - Init
     init() {
-        initialState = State()
+        initialState = State(
+            isBanner: false,
+            activitiesData: []
+        )
     }
     
 }
@@ -35,6 +45,12 @@ extension NewClubReactor {
         switch action {
         case let .imageDidSelect(data):
             return .just(.setImageData(data))
+        case .bannerDidTap:
+            return .just(.setIsBanner(true))
+        case .activityAppendButtonDidTap:
+            return .just(.setIsBanner(false))
+        case let .activityDeleteDidTap(index):
+            return .just(.removeImageData(index))
         }
         return .empty()
     }
@@ -47,7 +63,24 @@ extension NewClubReactor {
         
         switch mutation {
         case let .setImageData(data):
-            newState.imageData = data
+            if currentState.isBanner {
+                newState.imageData = data
+            } else {
+                if currentState.activitiesData.count > 3 {
+                    steps.accept(GCMSStep.alert(title: "GCMS",
+                                                message: "동아리 사진을 5개이상 추가할 수 없습니다!",
+                                                style: .alert,
+                                                actions: [
+                                                    .init(title: "확인", style: .cancel, handler: nil)
+                                                ]))
+                } else {
+                    newState.activitiesData.append(data)
+                }
+            }
+        case let .setIsBanner(status):
+            newState.isBanner = status
+        case let .removeImageData(index):
+            newState.activitiesData.remove(at: index)
         }
         
         return newState
