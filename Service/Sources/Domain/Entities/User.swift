d018479cf17cc6daf980c55909d8f96a67052296
baegteun <baegteun@gmail.com>
2022-03-05 16:16:26 +0900

    :sparkles: :: User Entity ID Property

--- Service/Sources/Domain/Entities/User.swift
+++ Service/Sources/Domain/Entities/User.swift
@@ -1,10 +1,12 @@
+import Foundation
 
 public struct User {
-    public init(profileImage: String, name: String) {
+    public init(id: UUID, profileImage: String, name: String) {
+        self.id = id
         self.profileImage = profileImage
         self.name = name
     }
-    
+    public let id: UUID
     public let profileImage: String
     public let name: String
 }
