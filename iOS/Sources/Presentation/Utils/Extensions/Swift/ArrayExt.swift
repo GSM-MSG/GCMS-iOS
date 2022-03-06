ee6fec92d976018d50de3ddc5049c2e2e8a49249
baegteun <baegteun@gmail.com>
2022-03-06 12:59:29 +0900

    :sparkles: :: Array removeDuplicates extension

---
+++ iOS/Sources/Presentation/Utils/Extensions/Swift/ArrayExt.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension Array where Element: Equatable {
+    mutating func removeDuplicates() {
+        var result = [Element]()
+        for value in self {
+            if !result.contains(value) {
+                result.append(value)
+            }
+        }
+        self = result
+    }
+}
