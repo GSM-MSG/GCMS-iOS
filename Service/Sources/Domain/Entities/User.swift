b2de905324114d6d17b1af80d6c1b16075fcecf4
baegteun <baegteun@gmail.com>
2022-03-06 12:59:05 +0900

    :sparkles: :: User Confirm Equatable

--- Service/Sources/Domain/Entities/User.swift
+++ Service/Sources/Domain/Entities/User.swift
@@ -1,12 +1,20 @@
 import Foundation
 
-public struct User {
-    public init(id: UUID, profileImage: String, name: String) {
+public struct User: Equatable {
+    public init(id: UUID, profileImage: String, name: String, grade: Int, class: Int, number: Int) {
         self.id = id
         self.profileImage = profileImage
         self.name = name
+        self.grade = grade
+        self.class = `class`
+        self.number = number
     }
+    
     public let id: UUID
     public let profileImage: String
     public let name: String
+    public let grade: Int
+    public let `class`: Int
+    public let number: Int
+    
 }
