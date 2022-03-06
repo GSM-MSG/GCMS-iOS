09097c18b72a0dd6a84489c9e4df3c45c19393ca
baegteun <baegteun@gmail.com>
2022-03-06 19:15:41 +0900

    :sparkles: :: JWTPlugin

---
+++ Service/Sources/Data/DataSource/Remote/Plugin/JWTPlugin.swift
@@ -0,0 +1,93 @@
+import Moya
+import Foundation
+
+protocol JWTTokenAuthorizable {
+    var jwtTokenType: JWTTokenType? { get }
+}
+
+enum JWTTokenType {
+    case none
+    
+    case accessToken
+    case refreshToken
+    
+    public var headerString: String {
+        switch self {
+        case .accessToken:
+            return "Authorization"
+        case .refreshToken:
+            return "refreshToken"
+        default:
+            return ""
+        }
+    }
+}
+
+final class JWTPlugin: PluginType {
+    
+    func prepare(
+        _ request: URLRequest,
+        target: TargetType
+    ) throws -> URLRequest {
+        guard let authorizable = target as? JWTTokenAuthorizable,
+              let tokenType = authorizable.jwtTokenType,
+              tokenType != .none
+        else { return request }
+        
+        var request = request
+        
+        let token = getToken(type: tokenType)
+        request.addValue(token, forHTTPHeaderField: tokenType.headerString)
+        return request
+    }
+    
+    func didReceive(
+        _ result: Result<Response, MoyaError>,
+        target: TargetType
+    ) {
+        switch result {
+        case let .success(res):
+            if let newToken = try? res.map(TokenDTO.self) {
+                self.setToken(token: newToken)
+            }
+        default:
+            break
+        }
+    }
+}
+
+private extension JWTPlugin {
+    func getToken(type: JWTTokenType) -> String {
+        switch type {
+        case .accessToken:
+            return getAccessToken()
+        case .refreshToken:
+            return getRefreshToken()
+        case .none:
+            return ""
+        }
+    }
+    
+    func getAccessToken() -> String {
+        do {
+            return try KeychainLocal.shared.fetchAccessToken()
+        } catch {
+            print(error.localizedDescription)
+            return ""
+        }
+    }
+    
+    func getRefreshToken() -> String {
+        do {
+            return try KeychainLocal.shared.fetchRefreshToken()
+        } catch {
+            print(error.localizedDescription)
+            return ""
+        }
+    }
+    
+    func setToken(token: TokenDTO) {
+        KeychainLocal.shared.saveAccessToken(token.accessToken)
+        KeychainLocal.shared.saveRefreshToken(token.refreshToken)
+    }
+}
