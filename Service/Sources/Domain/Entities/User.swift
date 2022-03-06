dc629e9e7afb322ff6f401b572145a85f7d5861b
baegteun <baegteun@gmail.com>
2022-03-06 16:24:56 +0900

    :sparkles: :: User Entitiy UUID to int

--- Service/Sources/Domain/Entities/User.swift
+++ Service/Sources/Domain/Entities/User.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public struct User: Equatable {
-    public init(id: UUID, profileImage: String, name: String, grade: Int, class: Int, number: Int) {
+    public init(id: Int, profileImage: String, name: String, grade: Int, class: Int, number: Int) {
         self.id = id
         self.profileImage = profileImage
         self.name = name
@@ -10,7 +10,7 @@ public struct User: Equatable {
         self.number = number
     }
     
-    public let id: UUID
+    public let id: Int
     public let profileImage: String
     public let name: String
     public let grade: Int
