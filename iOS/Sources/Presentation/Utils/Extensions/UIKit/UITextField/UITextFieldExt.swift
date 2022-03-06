a1968c8522252886a6d20f6bee3305c83b304bac
baegteun <baegteun@gmail.com>
2022-03-06 17:32:34 +0900

    :sparkles: ;:UITextField extension

--- iOS/Sources/Presentation/Utils/Extensions/UIKit/UITextField/UITextFieldExt.swift
+++ iOS/Sources/Presentation/Utils/Extensions/UIKit/UITextField/UITextFieldExt.swift
@@ -21,4 +21,32 @@ extension UITextField {
         rightView = spacer
         rightViewMode = .always
     }
+    func addLeftImage(image: UIImage, space: CGFloat = 10) {
+        let leftImage = UIImageView(image: image)
+        let view = UIView()
+        view.addSubViews(leftImage)
+        leftImage.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(space)
+            $0.centerY.equalToSuperview()
+        }
+        view.snp.makeConstraints {
+            $0.size.equalTo(34)
+        }
+        self.leftView = view
+        self.leftViewMode = .always
+    }
+    func addRightImage(image: UIImage, space: CGFloat = 10) {
+        let rightImage = UIImageView(image: image)
+        let view = UIView()
+        view.addSubViews(rightImage)
+        rightImage.snp.makeConstraints {
+            $0.trailing.equalToSuperview().offset(-space)
+            $0.centerY.equalToSuperview()
+        }
+        view.snp.makeConstraints {
+            $0.size.equalTo(34)
+        }
+        self.rightView = view
+        self.rightViewMode = .always
+    }
 } 
