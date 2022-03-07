1afda0cbc1ef1de3d2c0ec5b62ccb78564636365
baegteun <baegteun@gmail.com>
2022-03-07 23:51:03 +0900

    :sparkles: :: NewClubRequest DTO

--- Service/Sources/Data/DataSource/Remote/DataMapping/Club/NewClub/NewClubRequest.swift
+++ Service/Sources/Data/DataSource/Remote/DataMapping/Club/NewClub/NewClubRequest.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-public struct NewClubRequest {
-    public let photo: String
+public struct NewClubRequest: Encodable {
+    public let picture: String
     public let type: ClubType
     public let name: String
     public let description: String
     public let teacher: String
     public let head: String
     public let discord: String
-    public let clubPhoto: [String]
+    public let clubPicture: [String]
     public let clubMember: [Int]
 }
