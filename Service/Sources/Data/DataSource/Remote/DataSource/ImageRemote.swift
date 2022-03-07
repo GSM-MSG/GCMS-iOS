665b174d608e78421d0a13490ee041db0150b61d
baegteun <baegteun@gmail.com>
2022-03-07 23:54:59 +0900

    :sparkles: :: ImageRemote Capsule

--- Service/Sources/Data/DataSource/Remote/DataSource/ImageRemote.swift
+++ Service/Sources/Data/DataSource/Remote/DataSource/ImageRemote.swift
@@ -3,6 +3,7 @@ import Foundation
 
 final class ImageRemote: BaseRemote<ImageAPI> {
     static let shared = ImageRemote()
+    private override init() {}
     func updatePicture(data: Data, isTest: Bool = false) -> Single<String> {
         return request(.uploadImage(data), isTest: isTest)
             .map(String.self)
