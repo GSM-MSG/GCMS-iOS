fbf80aba796091152bbb65047d81a56d54cbc7a3
baegteun <baegteun@gmail.com>
2022-03-07 22:33:44 +0900

    :sparkles: :: UserSearchResponse

---
+++ Service/Sources/Data/DataSource/Remote/DataMapping/User/UserInfo/UserSearchResponse.swift
@@ -0,0 +1,10 @@
+
+struct UserSearchResponse: Codable {
+    let list: [UserDTO]
+}
+
+extension UserSearchResponse {
+    func toDomain() -> [User] {
+        return self.list.map { $0.toDomain() }
+    }
+}
