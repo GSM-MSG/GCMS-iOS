4bd889caeb5e56ac504481acd7a7a12ef61563d8
baegteun <baegteun@gmail.com>
2022-03-07 23:13:22 +0900

    :sparkles: :: userRepository userSearch testable

--- Service/Sources/Domain/Repositories/UserRepository.swift
+++ Service/Sources/Domain/Repositories/UserRepository.swift
@@ -4,8 +4,8 @@ import Foundation
 protocol UserRepository {
     func fetchUerInfo(isTest: Bool) -> Single<User>
     func fetchNoticeList(isTest: Bool) -> Single<[Notice]>
-    func editProfile(data: Data, isTest: Bool) -> Completable
+    func editProfile(url: String, isTest: Bool) -> Completable
     func acceptClub(clubId: Int, isTest: Bool) -> Completable
     func rejectClub(clubId: Int, isTest: Bool) -> Completable
-    func searchUser(query: String) -> Single<[User]>
+    func searchUser(query: String, isTest: Bool) -> Single<[User]>
 }
