005aecc0de028566f715362266122328a43d412d
baegteun <baegteun@gmail.com>
2022-03-07 23:13:36 +0900

    :sparkles: :: DefaultUserRepository

--- Service/Sources/Data/Repositories/DefaultUserRepository.swift
+++ Service/Sources/Data/Repositories/DefaultUserRepository.swift
@@ -3,28 +3,29 @@ import Foundation
 
 final class DefaultUserRepository: UserRepository {
     private let userRemote = UserRemote.shared
-    func fetchUerInfo(isTest: Bool) -> Single<User> {
+    func fetchUerInfo(isTest: Bool = false) -> Single<User> {
         return userRemote.userInfo(isTest: isTest)
             .map { $0.toDomain() }
     }
-    func fetchNoticeList(isTest: Bool) -> Single<[Notice]> {
+    func fetchNoticeList(isTest: Bool = false) -> Single<[Notice]> {
         return userRemote.notice(isTest: isTest)
             .map { $0.toDomain() }
     }
-    func editProfile(data: Data, isTest: Bool) -> Completable {
-        return .empty()
+    func editProfile(url: String, isTest: Bool = false) -> Completable {
+        return userRemote.editPicture(url: url, isTest: isTest)
     }
     
-    func acceptClub(clubId: Int, isTest: Bool) -> Completable {
-        return .empty()
+    func acceptClub(clubId: Int, isTest: Bool = false) -> Completable {
+        return userRemote.accept(clubId: clubId, isTest: isTest)
     }
     
-    func rejectClub(clubId: Int, isTest: Bool) -> Completable {
-        return .empty()
+    func rejectClub(clubId: Int, isTest: Bool = false) -> Completable {
+        return userRemote.reject(clubId: clubId, isTest: isTest)
     }
     
-    func searchUser(query: String) -> Single<[User]> {
-        return .just([])
+    func searchUser(query: String, isTest: Bool = false) -> Single<[User]> {
+        return userRemote.search(q: query, isTest: isTest)
+            .map { $0.toDomain() }
     }
     
 }
