a40532b6fd75181001c57dd918c765f69ab25a78
baegteun <baegteun@gmail.com>
2022-03-07 22:05:20 +0900

    :sparkles: :: DefaultAuthRepository

---
+++ Service/Sources/Data/Repositories/DefaultAuthRepository.swift
@@ -0,0 +1,27 @@
+import RxSwift
+import FirebaseMessaging
+final class DefaultAuthRepository: AuthRepository {
+    private let authRemote = AuthRemote.shared
+    private let keychainLocal = KeychainLocal.shared
+    func login(idToken: String, isTest: Bool = false) -> Completable {
+        return fetchDeviceToken()
+            .flatMapCompletable { deviceOoken in
+                return self.authRemote.login(req: .init(idToken: idToken, deviceToken: deviceOoken), isTest: isTest)
+                    .do(onSuccess: { tokenDTO in
+                        self.keychainLocal.saveAccessToken(tokenDTO.accessToken)
+                        self.keychainLocal.saveRefreshToken(tokenDTO.refreshToken)
+                    }).asCompletable()
+            }
+    }
+}
+
+private extension DefaultAuthRepository {
+    func fetchDeviceToken() -> Single<String> {
+        return Single<String>.create { single in
+            Messaging.messaging().token { token, _ in
+                single(.success(token ?? ""))
+            }
+            return Disposables.create()
+        }
+    }
+}
