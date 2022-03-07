b8ab8f4e0eddb2254e8ac5a971cda1a37a52dca4
baegteun <baegteun@gmail.com>
2022-03-07 22:45:13 +0900

    :sparkles: :: Notice Entity

---
+++ Service/Sources/Domain/Entities/Notice.swift
@@ -0,0 +1,17 @@
+
+public struct Notice: Equatable {
+    public init(id: Int, club: String, content: String, isInvite: Bool, clubId: Int) {
+        self.id = id
+        self.club = club
+        self.content = content
+        self.isInvite = isInvite
+        self.clubId = clubId
+    }
+    
+    public let id: Int
+    public let club: String
+    public let content: String
+    public let isInvite: Bool
+    public let clubId: Int
+    
+}
