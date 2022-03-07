f0e62ad7aca77afb1670700a7c492fbfd8b491ba
baegteun <baegteun@gmail.com>
2022-03-07 22:57:00 +0900

    :sparkles: :: UserRepository

--- Service/Sources/Domain/Repositories/UserRepository.swift
+++ Service/Sources/Domain/Repositories/UserRepository.swift
@@ -1,5 +1,11 @@
 import RxSwift
+import Foundation
 
 protocol UserRepository {
-    
+    func fetchUerInfo(isTest: Bool) -> Single<User>
+    func fetchNoticeList(isTest: Bool) -> Single<[Notice]>
+    func editProfile(data: Data, isTest: Bool) -> Completable
+    func acceptClub(clubId: Int, isTest: Bool) -> Completable
+    func rejectClub(clubId: Int, isTest: Bool) -> Completable
+    func searchUser(query: String) -> Single<[User]>
 }
