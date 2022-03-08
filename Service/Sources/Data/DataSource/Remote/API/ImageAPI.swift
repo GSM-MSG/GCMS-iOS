82cbf29bf7a519f0322e4a2a2330116b42b8a3c3
baegteun <baegteun@gmail.com>
2022-03-08 11:30:16 +0900

    :sparkles: :: ImageAPI - uploadPicutres

--- Service/Sources/Data/DataSource/Remote/API/ImageAPI.swift
+++ Service/Sources/Data/DataSource/Remote/API/ImageAPI.swift
@@ -3,6 +3,7 @@ import Foundation
 
 enum ImageAPI {
     case uploadImage(Data)
+    case uploadImages([Data])
 }
 
 extension ImageAPI: GCMSAPI {
@@ -13,28 +14,41 @@ extension ImageAPI: GCMSAPI {
         switch self {
         case .uploadImage:
             return "/upload/picture"
+        case .uploadImages:
+            return "/upload/pictures"
         }
     }
     var method: Moya.Method {
         switch self {
-        case .uploadImage:
+        case .uploadImage, .uploadImages:
             return .post
         }
     }
     var task: Task {
         switch self {
         case let .uploadImage(data):
+            let uuid = UUID().uuidString
             let multipart = MultipartFormData(
                 provider: .data(data),
-                name: "\(UUID().uuidString).png",
-                fileName: "\(UUID().uuidString).png"
+                name: uuid,
+                fileName: "\(uuid).png"
             )
             return .uploadMultipart([multipart])
+        case let .uploadImages(datas):
+            let multiparts = datas.map { data -> MultipartFormData in
+                let uuid = UUID().uuidString
+                return MultipartFormData(
+                    provider: .data(data),
+                    name: uuid,
+                    fileName: "\(uuid).png"
+                )
+            }
+            return .uploadMultipart(multiparts)
         }
     }
     var jwtTokenType: JWTTokenType? {
         switch self {
-        case .uploadImage:
+        case .uploadImage, .uploadImages:
             return .accessToken
         default:
             return JWTTokenType.none
