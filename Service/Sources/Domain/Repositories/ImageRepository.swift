9e423ff71758d9882126e042ead9f52da14f296e
baegteun <baegteun@gmail.com>
2022-03-08 11:31:13 +0900

    :sparkles: :: ImageRepository

---
+++ Service/Sources/Domain/Repositories/ImageRepository.swift
@@ -0,0 +1,7 @@
+import RxSwift
+ import Foundation
+
+ public protocol ImageRepository {
+     func uploadPicture(data: Data, isTest: Bool) -> Single<String>
+     func uploadPictures(datas: [Data], isTest: Bool) -> Single<[String]>
+ }
