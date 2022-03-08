5c60ea3091870559fe363623660a4635b35801c0
baegteun <baegteun@gmail.com>
2022-03-08 12:44:26 +0900

    :sparkles: :: User Entitiy - Append Property
    joinedMajorClub, joinedEditorialClub, joinedFreedomClub

--- Service/Sources/Domain/Entities/User.swift
+++ Service/Sources/Domain/Entities/User.swift
@@ -1,20 +1,25 @@
 import Foundation
 
 public struct User: Equatable {
-    public init(id: Int, profileImage: String, name: String, grade: Int, class: Int, number: Int) {
+    public init(id: Int, picture: String, name: String, grade: Int, class: Int, number: Int, joinedMajorClub: Int? = nil, joinedEditorialClub: [Int]? = nil, joinedFreedomClub: Int? = nil) {
         self.id = id
-        self.profileImage = profileImage
+        self.picture = picture
         self.name = name
         self.grade = grade
-        self.class = `class`
+        self.class = class
         self.number = number
+        self.joinedMajorClub = joinedMajorClub
+        self.joinedEditorialClub = joinedEditorialClub
+        self.joinedFreedomClub = joinedFreedomClub
     }
     
     public let id: Int
-    public let profileImage: String
+    public let picture: String
     public let name: String
     public let grade: Int
     public let `class`: Int
     public let number: Int
-    
+    public let joinedMajorClub: Int?
+    public let joinedEditorialClub: [Int]?
+    public let joinedFreedomClub: Int?
 }
