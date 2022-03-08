7c4491a832ab94c56c235f1ca46db87a1571e2d0
baegteun <baegteun@gmail.com>
2022-03-08 11:30:30 +0900

    :sparkles: :: ImageRemote

---
+++ Service/Sources/Data/DataSource/Remote/DataSource/ImageRemote.swift
@@ -0,0 +1,15 @@
+import RxSwift
+ import Foundation
+
+ final class ImageRemote: BaseRemote<ImageAPI> {
+     static let shared = ImageRemote()
+     private override init() {}
+     func uploadPicture(data: Data, isTest: Bool = false) -> Single<String> {
+         return request(.uploadImage(data), isTest: isTest)
+             .map(String.self)
+     }
+     func uploadPictures(datas: [Data], isTest: Bool = false) -> Single<[String]> {
+         return request(.uploadImages(datas), isTest: isTest)
+             .map([String].self)
+     }
+ }
