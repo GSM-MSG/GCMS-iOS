0d7d4dd0b268bcd277e97310fe12183914c4c273
baegteun <baegteun@gmail.com>
2022-03-10 08:21:17 +0900

    :sparkles: :: ClubRepository

--- Service/Sources/Domain/Repositories/ClubRepository.swift
+++ Service/Sources/Domain/Repositories/ClubRepository.swift
@@ -3,4 +3,5 @@ import RxSwift
 public protocol ClubRepository {
     func createNewClub(req: NewClubRequest, isTest: Bool) -> Completable
     func fetchManagementClub(isTest: Bool) -> Single<[ClubList]>
+    func fetchClubList(type: ClubType, isTest: Bool) -> Single<[ClubList]>
 }
