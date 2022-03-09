66e88fbce8b36e17764a1c8d4a273622a9c59e3e
baegteun <baegteun@gmail.com>
2022-03-09 15:13:19 +0900

    :sparkles: :: MyPage Indicator

--- iOS/Sources/Presentation/Scene/Main/MyPage/Reactor/MyPageReactor.swift
+++ iOS/Sources/Presentation/Scene/Main/MyPage/Reactor/MyPageReactor.swift
@@ -86,7 +86,7 @@ private extension MyPageReactor {
                 .init(id: 2, bannerUrl: "https://avatars.githubusercontent.com/u/74440939?v=4", title: "ㅁㄴㅇ", type: .editorial),
                 .init(id: 3, bannerUrl: "https://avatars.githubusercontent.com/u/74440939?v=4", title: "ㅍ", type: .editorial)
             ])),
-            .just(.setMajorClub(.init(id: 2, bannerUrl: "https://avatars.githubusercontent.com/u/80966659?v=4", title: "MAJOR", type: .major))),g
+            .just(.setMajorClub(.init(id: 2, bannerUrl: "https://avatars.githubusercontent.com/u/80966659?v=4", title: "MAJOR", type: .major))),
             .just(.setFreedomClub(.init(id: 3, bannerUrl: "https://camo.githubusercontent.com/9ed64b042a76b8a97016e877cbaee0d6df224a148034afef658d841cf0cd1791/68747470733a2f2f63756c746f667468657061727479706172726f742e636f6d2f706172726f74732f68642f6c6170746f705f706172726f742e676966", title: "FREEDOM", type: .freedom)))
         ])
     }
