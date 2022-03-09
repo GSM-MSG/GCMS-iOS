c47f382c8e968e783f97d132d5839b2d8f440a20
baegteun <baegteun@gmail.com>
2022-03-09 15:12:59 +0900

    :sparkles: :: MyPage Indicator

--- iOS/Sources/Presentation/Scene/Main/MyPage/Reactor/MyPageReactor.swift
+++ iOS/Sources/Presentation/Scene/Main/MyPage/Reactor/MyPageReactor.swift
@@ -14,23 +14,27 @@ final class MyPageReactor: Reactor, Stepper {
     enum Action {
         case viewDidLoad
         case clubManageButtonDidTap
+        case updateLoading(Bool)
     }
     enum Mutation {
         case setEditorialClubList([ClubList])
         case setMajorClub(ClubList)
         case setFreedomClub(ClubList)
+        case setIsLoading(Bool)
     }
     struct State {
         var editorialClubList: [ClubList]
         var majorClub: ClubList?
         var freedomClub: ClubList?
+        var isLoading: Bool
     }
     let initialState: State
     
     // MARK: - Init
     init() {
         initialState = State(
-            editorialClubList: []
+            editorialClubList: [],
+            isLoading: false
         )
     }
     
@@ -45,6 +49,8 @@ extension MyPageReactor {
         case .clubManageButtonDidTap:
             steps.accept(GCMSStep.clubManagementIsRequired)
 //            return clubManageButtonDidTap()
+        case let .updateLoading(load):
+            return .just(.setIsLoading(load))
         }
         return .empty()
     }
@@ -62,6 +68,8 @@ extension MyPageReactor {
             newState.majorClub = club
         case let .setFreedomClub(club):
             newState.freedomClub = club
+        case let .setIsLoading(load):
+            newState.isLoading = load
         }
         
         return newState
@@ -78,7 +86,7 @@ private extension MyPageReactor {
                 .init(id: 2, bannerUrl: "https://avatars.githubusercontent.com/u/74440939?v=4", title: "ㅁㄴㅇ", type: .editorial),
                 .init(id: 3, bannerUrl: "https://avatars.githubusercontent.com/u/74440939?v=4", title: "ㅍ", type: .editorial)
             ])),
-            .just(.setMajorClub(.init(id: 2, bannerUrl: "https://avatars.githubusercontent.com/u/80966659?v=4", title: "MAJOR", type: .major))),
+            .just(.setMajorClub(.init(id: 2, bannerUrl: "https://avatars.githubusercontent.com/u/80966659?v=4", title: "MAJOR", type: .major))),g
             .just(.setFreedomClub(.init(id: 3, bannerUrl: "https://camo.githubusercontent.com/9ed64b042a76b8a97016e877cbaee0d6df224a148034afef658d841cf0cd1791/68747470733a2f2f63756c746f667468657061727479706172726f742e636f6d2f706172726f74732f68642f6c6170746f705f706172726f742e676966", title: "FREEDOM", type: .freedom)))
         ])
     }
