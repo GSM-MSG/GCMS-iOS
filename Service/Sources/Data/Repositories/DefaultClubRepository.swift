549bb941db82faf4247e27bcf84f02164a3ca58f
baegteun <baegteun@gmail.com>
2022-03-10 08:21:28 +0900

    :sparkles: :: ClubRepository Impl

--- Service/Sources/Data/Repositories/DefaultClubRepository.swift
+++ Service/Sources/Data/Repositories/DefaultClubRepository.swift
@@ -3,11 +3,15 @@ import RxSwift
 final class DefaultClubRepository: ClubRepository {
     private let clubRemote = ClubRemote.shared
     
-    func createNewClub(req: NewClubRequest, isTest: Bool) -> Completable {
+    func createNewClub(req: NewClubRequest, isTest: Bool = false) -> Completable {
         clubRemote.newClub(req: req, isTest: isTest)
     }
-    func fetchManagementClub(isTest: Bool) -> Single<[ClubList]> {
+    func fetchManagementClub(isTest: Bool = false) -> Single<[ClubList]> {
         clubRemote.managementClub(isTest: isTest)
+            .map { $0.toDomain() }
+    }
+    func fetchClubList(type: ClubType, isTest: Bool = false) -> Single<[ClubList]> {
+        clubRemote.fetchClubList(type: type, isTest: isTest)
             .map { $0.toDomain() }
     }
 }
