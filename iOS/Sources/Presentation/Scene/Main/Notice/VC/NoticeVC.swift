646b5097271f7405f67a09e101686788d743d171
sunghun <sunghun4777@gmail.com>
2022-03-10 19:05:46 +0900

    :lipstick: NoticeVC
    NoticeVC TextView placeholder

--- iOS/Sources/Presentation/Scene/Main/Notice/VC/NoticeVC.swift
+++ iOS/Sources/Presentation/Scene/Main/Notice/VC/NoticeVC.swift
@@ -1,14 +1,22 @@
 import UIKit
 import SnapKit
+import RxDataSources
 
 final class NoticeVC: BaseVC<NoticeReactor> {
     // MARK: - Properties
-    private let contentTextView = UITextView()
+    private let contentTextView = UITextView().then {
+        $0.backgroundColor = GCMSAsset.Colors.gcmsBackgroundColor.color
+        $0.text = "공지할 내용을 입력해주세요."
+        $0.textColor = GCMSAsset.Colors.gcmsGray4.color
+        $0.font = UIFont(font: GCMSFontFamily.Inter.bold, size: 13)
+    }
     private let completeButton = UIButton().then {
         $0.setTitle("완료", for: .normal)
         $0.setTitleColor(GCMSAsset.Colors.gcmsGray1.color, for: .normal)
         $0.titleLabel?.textAlignment = .center
         $0.titleLabel?.font = UIFont(font: GCMSFontFamily.Inter.semiBold, size: 18)
+        $0.layer.cornerRadius = 5
+        $0.backgroundColor = GCMSAsset.Colors.gcmsMainColor.color
     }
     
     // MARK: - UI
@@ -17,12 +25,12 @@ final class NoticeVC: BaseVC<NoticeReactor> {
     }
     override func setLayout() {
         completeButton.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview().inset(15)
+            $0.leading.trailing.equalToSuperview().inset(45)
             $0.bottom.equalToSuperview().offset(-30)
             $0.height.equalTo(50)
         }
         contentTextView.snp.makeConstraints {
-            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(15)
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(30)
             $0.bottom.equalTo(completeButton.snp.top).offset(-30)
         }
     }
@@ -34,4 +42,31 @@ final class NoticeVC: BaseVC<NoticeReactor> {
     override func configureNavigation() {
         self.navigationItem.configTitle(title: "공지")
     }
+    
+    // MARK: - Reactor
+    
+    override func bindView(reactor: NoticeReactor) {
+        contentTextView.rx.didBeginEditing
+            .asObservable()
+            .withUnretained(self)
+            .bind { owner, _ in
+                if owner.contentTextView.textColor == GCMSAsset.Colors.gcmsGray4.color {
+                    owner.contentTextView.text = nil
+                    owner.contentTextView.textColor = GCMSAsset.Colors.gcmsGray1.color
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        contentTextView.rx.didEndEditing
+            .asObservable()
+            .withUnretained(self)
+            .bind { owner, _ in
+                if owner.contentTextView.text.isEmpty {
+                    owner.contentTextView.text = "공지할 내용을 입력해주세요."
+                    owner.contentTextView.textColor = GCMSAsset.Colors.gcmsGray4.color
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+    
 }
