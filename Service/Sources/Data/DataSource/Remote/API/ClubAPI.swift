619c3fc6b8068d108232f6ea4f38c275c186372d
baegteun <baegteun@gmail.com>
2022-03-18 08:45:33 +0900

    :sparkles: :: ClubAPI Routing, Requesting

--- Service/Sources/Data/DataSource/Remote/API/ClubAPI.swift
+++ Service/Sources/Data/DataSource/Remote/API/ClubAPI.swift
@@ -2,6 +2,17 @@ import Moya
 
 enum ClubAPI {
     case clubList(type: ClubType)
+    case clubDetail(query: ClubRequestComponent)
+    case createNewClub(req: CreateNewClubRequest)
+    case updateClub(query: ClubRequestComponent, req: CreateNewClubRequest)
+    case deleteClub(query: ClubRequestComponent)
+    case clubMember(query: ClubRequestComponent)
+    case clubApplicant(query: ClubRequestComponent)
+    case userAccept(query: ClubRequestComponent, userId: String)
+    case userReject(query: ClubRequestComponent, userId: String)
+    case clubOpen(query: ClubRequestComponent)
+    case clubClose(query: ClubRequestComponent)
+    case userKick(query: ClubRequestComponent, userId: String)
 }
 
 extension ClubAPI: GCMSAPI {
@@ -12,12 +23,36 @@ extension ClubAPI: GCMSAPI {
         switch self {
         case .clubList:
             return "/list"
+        case .clubDetail:
+            return "/detail"
+        case .createNewClub, .updateClub, .deleteClub:
+            return "/"
+        case .clubMember:
+            return "/members"
+        case .clubApplicant:
+            return "/applicant"
+        case .userAccept:
+            return "/accept"
+        case .userReject:
+            return "/reject"
+        case .clubOpen:
+            return "/open"
+        case .clubClose:
+            return "/close"
+        case .userKick:
+            return "/kick"
         }
     }
     var method: Method {
         switch self {
-        case .clubList:
+        case .clubList, .clubDetail, .createNewClub, .clubMember, .clubApplicant:
             return .get
+        case .userAccept, .userReject:
+            return .post
+        case .updateClub, .clubOpen, .clubClose:
+            return .put
+        case .deleteClub, .userKick:
+            return .delete
         }
     }
     var task: Task {
@@ -26,14 +61,40 @@ extension ClubAPI: GCMSAPI {
             return .requestParameters(parameters: [
                 "type": type.rawValue
             ], encoding: URLEncoding.queryString)
+        case let .clubDetail(q), let .clubMember(q), let .clubApplicant(q):
+            return .requestParameters(parameters: [
+                "q": q.name,
+                "type": q.type
+            ], encoding: URLEncoding.queryString)
+        case let .createNewClub(req):
+            return .requestJSONEncodable(req)
+        case let .deleteClub(query), let .clubOpen(query), let .clubClose(query):
+            return .requestJSONEncodable(query)
+        case let .userAccept(query, userId), let .userReject(query, userId), let .userKick(query, userId):
+            return .requestParameters(parameters: [
+                "q": query.name,
+                "type": query.type.rawValue,
+                "userId": userId
+            ], encoding: JSONEncoding.default)
+        case let .updateClub(query, req):
+            return .requestParameters(parameters: [
+                "q": query.name,
+                "type": query.type.rawValue,
+                "title": req.title,
+                "description": req.description,
+                "bannerUrl": req.bannerUrl,
+                "contact": req.contact,
+                "relatedLink": req.relatedLink,
+                "teacher": req.teacher,
+                "activities": req.activities,
+                "member": req.member
+            ], encoding: JSONEncoding.default)
         }
     }
     var jwtTokenType: JWTTokenType? {
         switch self {
-        case .clubList:
-            return .accessToken
         default:
-            return JWTTokenType.none
+            return .accessToken
         }
     }
 }
