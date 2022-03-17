c979cc7942372c5edd64775134be55f028607e77
baegteun <baegteun@gmail.com>
2022-03-18 08:45:33 +0900

    :sparkles: :: ClubAPI Routing, Requesting

---
+++ Service/Sources/Data/DataSource/Remote/DataMapping/Club/NewClub/CreateNewClubRequest.swift
@@ -0,0 +1,10 @@
+struct CreateNewClubRequest: Encodable {
+    let title: String
+    let description: String
+    let bannerUrl: String
+    let contact: String
+    let relatedLink: [RelatedLink]
+    let teacher: String?
+    let activities: [String]
+    let member: [String]
+}
