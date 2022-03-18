2259634e1229f23bfd4bf7e7d04d4caa3d48bdbb
baegteun <baegteun@gmail.com>
2022-03-18 11:26:58 +0900

    :sparkles: :: DetailClubResponse

--- Service/Sources/Data/DataSource/Remote/DataMapping/Club/DetailClub/DetailClubResponse.swift
+++ Service/Sources/Data/DataSource/Remote/DataMapping/Club/DetailClub/DetailClubResponse.swift
@@ -3,5 +3,31 @@ struct DetailClubResponse: Codable {
     let bannerUrl: String
     let title: String
     let description: String
+    let contact: String
     let head: UserDTO
+    let relatedLink: [RelatedLink]
+    let scope: MemberScope
+    let isApplied: Bool
+    let isOpen: Bool
+    let activities: [String]
+    let teacher: String?
+}
+
+extension DetailClubResponse {
+    func toDomain() -> Club {
+        return .init(
+            type: type,
+            bannerUrl: bannerUrl,
+            title: title,
+            description: description,
+            contact: contact,
+            head: head.toDomain(),
+            relatedLink: relatedLink,
+            scope: scope,
+            isApplied: isApplied,
+            isOpen: isOpen,
+            activities: activities,
+            teacher: teacher
+        )
+    }
 }
