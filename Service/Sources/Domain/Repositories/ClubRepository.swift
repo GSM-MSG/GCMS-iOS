e689fe69731f8cfadacec100064b5cb59c9f07e1
baegteun <baegteun@gmail.com>
2022-03-18 11:28:17 +0900

    :sparkles: :: ClubRepository

--- Service/Sources/Domain/Repositories/ClubRepository.swift
+++ Service/Sources/Domain/Repositories/ClubRepository.swift
@@ -1,5 +1,16 @@
 import RxSwift
 
 public protocol ClubRepository {
-    
+    func fetchClubList(type: ClubType, isTest: Bool) -> Single<[ClubList]>
+    func fetchDetailClub(query: ClubRequestComponent, isTest: Bool) -> Single<Club>
+    func createNewClub(req: NewClubRequest, isTest: Bool) -> Completable
+    func updateClub(query: ClubRequestComponent, req: NewClubRequest, isTest: Bool) -> Completable
+    func deleteClub(query: ClubRequestComponent, isTest: Bool) -> Completable
+    func fetchClubMember(query: ClubRequestComponent, isTest: Bool) -> Single<[User]>
+    func fetchClubApplicant(query: ClubRequestComponent, isTest: Bool) -> Single<[User]>
+    func userAccept(query: ClubRequestComponent, userId: String, isTest: Bool) -> Completable
+    func userReject(query: ClubRequestComponent, userId: String, isTest: Bool) -> Completable
+    func clubOpen(query: ClubRequestComponent, isTest: Bool) -> Completable
+    func clubClose(query: ClubRequestComponent, isTest: Bool) -> Completable
+    func userKick(query: ClubRequestComponent, userId: String, isTest: Bool) -> Completable
 }
