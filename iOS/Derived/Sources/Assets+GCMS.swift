d32577b4f19b2319da87d5986bdaf92acccee79d
baegteun <baegteun@gmail.com>
2022-03-25 11:27:46 +0900

    :bento: :: Theme Color Asset 추가

--- iOS/Derived/Sources/Assets+GCMS.swift
+++ iOS/Derived/Sources/Assets+GCMS.swift
@@ -21,6 +21,7 @@ public enum GCMSAsset {
     public static let gcmsBackgroundColor = GCMSColors(name: "GCMS_BackgroundColor")
     public static let gcmsMainColor = GCMSColors(name: "GCMS_MainColor")
     public static let gcmsOnBoardingMainColor = GCMSColors(name: "GCMS_OnBoardingMainColor")
+    public static let gcmsThemeColor = GCMSColors(name: "GCMS_ThemeColor")
     public static let gcmsGray1 = GCMSColors(name: "GCMS_Gray1")
     public static let gcmsGray2 = GCMSColors(name: "GCMS_Gray2")
     public static let gcmsGray3 = GCMSColors(name: "GCMS_Gray3")
