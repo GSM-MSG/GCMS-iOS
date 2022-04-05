2ec990ef02e136d5767303b2e64058d4d4e82be7
baegteun <baegteun@gmail.com>
2022-04-05 19:39:54 +0900

    :sparkles: :: Label [선택] option function

--- iOS/Sources/Presentation/Utils/Extensions/UIKit/UILabel/UILabelExt.swift
+++ iOS/Sources/Presentation/Utils/Extensions/UIKit/UILabel/UILabelExt.swift
@@ -1,10 +1,10 @@
 import UIKit
 
 extension UILabel {
-    func appendRequired() {
-        let str = NSMutableAttributedString(string: "\(self.text ?? "")(필수)")
-        str.setFontForText(textToFind: "(필수)", withFont: .init(font: GCMSFontFamily.Inter.semiBold, size: 13) ?? .init())
-        str.setColorForText(textToFind: "(필수)", withColor: GCMSAsset.Colors.gcmsGray4.color)
+    func appendSelection() {
+        let str = NSMutableAttributedString(string: "\(self.text ?? "") [선택]")
+        str.setFontForText(textToFind: "[선택]", withFont: .init(font: GCMSFontFamily.Inter.semiBold, size: 11) ?? .init())
+        str.setColorForText(textToFind: "[선택]", withColor: GCMSAsset.Colors.gcmsGray1.color)
         self.attributedText = str
     }
 }
