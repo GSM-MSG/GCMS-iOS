35850515e8053ad1e22845f01c9c2da8f02712d2
baegteun <baegteun@gmail.com>
2022-04-12 23:48:58 +0900

    :sparkles: :: DetailClubDTO

---
+++ Service/Sources/Data/DataSource/Remote/DataMapping/Club/DetailClub/DetailClubDTO.swift
@@ -0,0 +1,10 @@
+struct DetailClubDTO: Codable {
+    let title: String
+    let description: String
+    let contact: String
+    let teacher: String
+    let isOpend: Bool
+    let type: ClubType
+    let bannerUrl: String
+    let relatedLink: [RelatedLinkDTO]
+}
--- Service/Sources/Data/DataSource/Remote/DataMapping/Club/DetailClub/DetailClubResponse.swift
+++ Service/Sources/Data/DataSource/Remote/DataMapping/Club/DetailClub/DetailClubResponse.swift
@@ -1,32 +1,31 @@
 struct DetailClubResponse: Codable {
-    let type: ClubType
-    let bannerUrl: String
-    let title: String
-    let description: String
-    let contact: String
+    let club: DetailClubDTO
     let head: UserDTO
-    let relatedLink: RelatedLinkDTO
     let member: [UserDTO]
     let scope: MemberScope
     let isApplied: Bool
-    let isOpen: Bool
     let activities: [String]
     let teacher: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case club, head, member, scope, isApplied, teacher
+        case activities = "activityUrls"
+    }
 }
 
 extension DetailClubResponse {
     func toDomain() -> Club {
         return .init(
-            type: type,
-            bannerUrl: bannerUrl,
-            title: title,
-            description: description,
-            contact: contact,
+            type: club.type,
+            bannerUrl: club.bannerUrl,
+            title: club.title,
+            description: club.description,
+            contact: club.contact,
             head: head.toDomain(),
-            relatedLink: relatedLink.toDomain(),
+            relatedLink: club.relatedLink.map { $0.toDomain() },
             scope: scope,
             isApplied: isApplied,
-            isOpen: isOpen,
+            isOpen: club.isOpend,
             activities: activities,
             member: member.map { $0.toDomain() },
             teacher: teacher
