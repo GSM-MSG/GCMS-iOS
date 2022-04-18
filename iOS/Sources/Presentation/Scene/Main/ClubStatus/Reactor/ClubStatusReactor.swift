afd7b7cdd5db50e24b4988725da9cc8174f603d3
baegteun <baegteun@gmail.com>
2022-04-18 23:33:16 +0900

    :sparkles: :: REactor DI

--- iOS/Sources/Presentation/Scene/Main/ClubStatus/Reactor/ClubStatusReactor.swift
+++ iOS/Sources/Presentation/Scene/Main/ClubStatus/Reactor/ClubStatusReactor.swift
@@ -13,12 +13,17 @@ final class ClubStatusReactor: Reactor, Stepper {
     // MARK: - Reactor
     enum Action {
         case updateLoading(Bool)
+        case viewDidLoad
     }
     enum Mutation {
         case setIsLoading(Bool)
+        case setMembers([Member])
+        case setApplicants([User])
     }
     struct State {
         var isLoading: Bool
+        var members: [Member]
+        var applicants: [User]
     }
     let initialState: State
     private let query: ClubRequestQuery
@@ -36,7 +41,9 @@ final class ClubStatusReactor: Reactor, Stepper {
         fetchClubApplicantUseCase: FetchClubApplicantUseCase
     ) {
         initialState = State(
-            isLoading: false
+            isLoading: false,
+            members: [],
+            applicants: []
         )
         self.query = query
         self.clubOpenUseCase = clubOpenUseCase
@@ -53,6 +60,8 @@ extension ClubStatusReactor {
         switch action {
         case let .updateLoading(load):
             return .just(.setIsLoading(load))
+        case .viewDidLoad:
+            return viewDidLoad()
         }
         return .empty()
     }
@@ -66,6 +75,10 @@ extension ClubStatusReactor {
         switch mutation {
         case let .setIsLoading(load):
             newState.isLoading = load
+        case let .setMembers(member):
+            newState.members = member
+        case let .setApplicants(users):
+            newState.applicants = users
         }
         
         return newState
@@ -74,5 +87,23 @@ extension ClubStatusReactor {
 
 // MARK: - Method
 private extension ClubStatusReactor {
-    
+    func viewDidLoad() -> Observable<Mutation> {
+        let startLoading = Observable.just(Mutation.setIsLoading(true))
+        let members: [Member] = [
+            .dummy,
+            .dummy,
+            .dummy
+        ]
+        let applicants: [User] = [
+            .dummy,
+            .dummy
+        ]
+        let stopLoading = Observable.just(Mutation.setIsLoading(false))
+        return .concat([
+            startLoading,
+            .just(.setMembers(members)),
+            .just(.setApplicants(applicants)),
+            stopLoading
+        ])
+    }
 }
