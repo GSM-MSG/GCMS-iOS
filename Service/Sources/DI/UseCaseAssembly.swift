ce9016dbe0108b13efed2dedd06c5a6ec59ea07c
baegteun <baegteun@gmail.com>
2022-04-18 14:55:37 +0900

    :sparkles: :: UseCase Club Assembly

--- Service/Sources/DI/UseCaseAssembly.swift
+++ Service/Sources/DI/UseCaseAssembly.swift
@@ -3,6 +3,7 @@ import Swinject
 public final class UseCaseAssembly: Assembly {
     public init() {}
     public func assemble(container: Container) {
+        // MARK: - Auth
         container.register(LoginUseCase.self) { r in
             return LoginUseCase(
                 authRepository: r.resolve(AuthRepository.self)!
@@ -36,6 +37,73 @@ public final class UseCaseAssembly: Assembly {
         container.register(LogoutUseCase.self) { r in
             return LogoutUseCase(
                 authRepository: r.resolve(AuthRepository.self)!
+            )
+        }.inObjectScope(.container)
+        
+        // MARK: - Club
+        container.register(ClubOpenUseCase.self) { r in
+            return ClubOpenUseCase(
+                clubRepository: r.resolve(ClubRepository.self)!
+            )
+        }.inObjectScope(.container)
+        
+        container.register(ClubCloseUseCase.self) { r in
+            return ClubCloseUseCase(
+                clubRepository: r.resolve(ClubRepository.self)!
+            )
+        }.inObjectScope(.container)
+        
+        container.register(CreateNewClubUseCase.self) { r in
+            return CreateNewClubUseCase(
+                clubRepository: r.resolve(ClubRepository.self)!
+            )
+        }.inObjectScope(.container)
+        
+        container.register(DeleteClubUseCase.self) { r in
+            return DeleteClubUseCase(
+                clubRepository: r.resolve(ClubRepository.self)!
+            )
+        }.inObjectScope(.container)
+        
+        container.register(FetchClubLiseUseCase.self) { r in
+            return FetchClubLiseUseCase(
+                clubRepository: r.resolve(ClubRepository.self)!
+            )
+        }.inObjectScope(.container)
+        
+        container.register(FetchClubMemberUseCase.self) { r in
+            return FetchClubMemberUseCase(
+                clubRepository: r.resolve(ClubRepository.self)!
+            )
+        }.inObjectScope(.container)
+        
+        container.register(FetchClubApplicantUseCase.self) { r in
+            return FetchClubApplicantUseCase(
+                clubRepository: r.resolve(ClubRepository.self)!
+            )
+        }.inObjectScope(.container)
+        
+        container.register(FetchDetailClubUseCase.self) { r in
+            return FetchDetailClubUseCase(
+                clubRepository: r.resolve(ClubRepository.self)!
+            )
+        }.inObjectScope(.container)
+        
+        container.register(UserAcceptUseCase.self) { r in
+            return UserAcceptUseCase(
+                clubRepository: r.resolve(ClubRepository.self)!
+            )
+        }.inObjectScope(.container)
+        
+        container.register(UserRejectUseCase.self) { r in
+            return UserRejectUseCase(
+                clubRepository: r.resolve(ClubRepository.self)!
+            )
+        }.inObjectScope(.container)
+        
+        container.register(UserKickUseCase.self) { r in
+            return UserKickUseCase(
+                clubRepository: r.resolve(ClubRepository.self)!
             )
         }.inObjectScope(.container)
     }
