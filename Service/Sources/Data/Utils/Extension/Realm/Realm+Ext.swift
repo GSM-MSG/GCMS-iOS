f7de1cd1f7b074890a82f4e0a8c9649fc49caa63
baegteun <baegteun@gmail.com>
2022-05-31 09:36:06 +0900

    :sparkles: :: RealmTask

---
+++ Service/Sources/Data/Utils/Extension/Realm/Realm+Ext.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+import RealmSwift
+
+extension Realm {
+    func safeWrite(_ block: (() throws -> Void)) throws {
+        if isInWriteTransaction {
+            try block()
+        } else {
+            try write(block)
+        }
+    }
+}
