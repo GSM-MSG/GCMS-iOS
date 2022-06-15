62c1949ebca18fbdc5ae531dbb3d12d701db169e
sunghun <sunghun4777@gmail.com>
2022-06-15 20:58:06 +0900

    🔥 ApeendMebmerUI

--- iOS/Sources/Presentation/Scene/Main/UpdateClub/VC/SecondUpdateClubVC.swift
+++ iOS/Sources/Presentation/Scene/Main/UpdateClub/VC/SecondUpdateClubVC.swift
@@ -36,23 +36,6 @@ final class SecondUpdateClubVC: BaseVC<UpdateClubReactor> {
         $0.register(cellType: ClubActivityCell.self)
     }
     private let clubActivityAppendButton = NewClubAppendButton()
-    private let clubMemberHeaderLabel = HeaderLabel(title: "동아리 구성원").then {
-        $0.appendSelection()
-    }
-    private let clubMemberCollectionView = UICollectionView(frame: .zero, collectionViewLayout: .init()).then {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.itemSize = .init(width: 61, height: 82)
-        $0.collectionViewLayout = layout
-        $0.backgroundColor = .clear
-        $0.register(cellType: ClubMemberCell.self)
-    }
-    private let clubMemberCountLabel = UILabel().then {
-        $0.text = "0명"
-        $0.textColor = GCMSAsset.Colors.gcmsGray3.color
-        $0.font = UIFont(font: GCMSFontFamily.Inter.semiBold, size: 12)
-    }
-    private let clubMemberAppendButton = NewClubAppendButton()
     private let completeButton = UIButton().then {
         $0.setTitle("완료", for: .normal)
         $0.setTitleColor(GCMSAsset.Colors.gcmsGray1.color, for: .normal)
@@ -81,7 +64,7 @@ final class SecondUpdateClubVC: BaseVC<UpdateClubReactor> {
     }
     override func addView() {
         view.addSubViews(scrollView, completeButton)
-        scrollView.addSubViews(progressBar, bannerImageView, bannerHeaderLabel, clubActivitiesHeaderLabel, clubActivitiesCollectionView, clubActivityAppendButton, clubMemberHeaderLabel, clubMemberCountLabel, clubMemberAppendButton, clubMemberCollectionView)
+        scrollView.addSubViews(progressBar, bannerImageView, bannerHeaderLabel, clubActivitiesHeaderLabel, clubActivitiesCollectionView, clubActivityAppendButton)
     }
     override func setLayout() {
         scrollView.snp.makeConstraints {
@@ -117,25 +100,6 @@ final class SecondUpdateClubVC: BaseVC<UpdateClubReactor> {
             $0.centerY.equalTo(clubActivitiesHeaderLabel)
             $0.size.equalTo(Metric.buttonSize)
         }
-        clubMemberCollectionView.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview().inset(Metric.horizontalMargin)
-            $0.top.equalTo(clubActivitiesCollectionView.snp.bottom).offset(Metric.verticalSpacing)
-            $0.height.equalTo(82)
-            $0.bottom.equalToSuperview().offset(-71)
-        }
-        clubMemberHeaderLabel.snp.makeConstraints {
-            $0.leading.equalTo(clubMemberCollectionView)
-            $0.bottom.equalTo(clubMemberCollectionView.snp.top).offset(-8)
-        }
-        clubMemberCountLabel.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.centerY.equalTo(clubMemberHeaderLabel)
-        }
-        clubMemberAppendButton.snp.makeConstraints {
-            $0.trailing.equalTo(clubMemberCollectionView)
-            $0.bottom.equalTo(clubMemberCollectionView.snp.top).offset(-8)
-            $0.size.equalTo(Metric.buttonSize)
-        }
         completeButton.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview()
             $0.bottom.equalToSuperview()
@@ -166,17 +130,6 @@ final class SecondUpdateClubVC: BaseVC<UpdateClubReactor> {
         clubActivitiesCollectionView.rx.itemSelected
             .map(\.row)
             .map(Reactor.Action.activityDeleteDidTap)
-            .bind(to: reactor.action)
-            .disposed(by: disposeBag)
-        
-        clubMemberAppendButton.rx.tap
-            .map { Reactor.Action.memberAppendButtonDidTap }
-            .bind(to: reactor.action)
-            .disposed(by: disposeBag)
-        
-        clubMemberCollectionView.rx.itemSelected
-            .map(\.row)
-            .map(Reactor.Action.memberRemove)
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
         
@@ -211,14 +164,6 @@ final class SecondUpdateClubVC: BaseVC<UpdateClubReactor> {
             .map(\.activitiesData)
             .map { [ClubActivitySection.init(items: $0)] }
             .bind(to: clubActivitiesCollectionView.rx.items(dataSource: activityDS))
-            .disposed(by: disposeBag)
-        
-        sharedState
-            .map(\.members)
-            .do(onNext: { [weak self] item in
-                self?.clubMemberCountLabel.text = "\(item.count)명"
-            }).map { [ClubMemberSection.init(header: "", items: $0)] }
-            .bind(to: clubMemberCollectionView.rx.items(dataSource: memberDS))
             .disposed(by: disposeBag)
         
         sharedState
