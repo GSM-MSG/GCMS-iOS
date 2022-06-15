1460e2aad0ce547dcd6ef43d4ec4d620f2680def
sunghun <sunghun4777@gmail.com>
2022-06-15 17:51:14 +0900

    🔥 remove Club in GuestCode

--- Service/Sources/Domain/Repositories/ClubRepository.swift
+++ Service/Sources/Domain/Repositories/ClubRepository.swift
@@ -2,9 +2,7 @@ import RxSwift
 
 public protocol ClubRepository {
     func fetchClubList(type: ClubType) -> Observable<[ClubList]>
-    func fetchGuestClubList(type: ClubType) -> Observable<[ClubList]>
     func fetchDetailClub(query: ClubRequestQuery) -> Single<Club>
-    func fetchGuestDetailClub(query: ClubRequestQuery) -> Single<Club>
     func createNewClub(req: NewClubRequest) -> Completable
     func updateClub(req: UpdateClubRequest) -> Completable
     func deleteClub(query: ClubRequestQuery) -> Completable
