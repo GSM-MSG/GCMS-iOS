bb24ccd435454db024b75851bd8fc6d251de5333
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2022-06-30 10:31:52 +0900

    :wrench: conflicts
    conflicts

--- iOS/Sources/Presentation/Scene/Auth/OnBoarding/Reactor/OnBoardingReactor.swift
+++ iOS/Sources/Presentation/Scene/Auth/OnBoarding/Reactor/OnBoardingReactor.swift
@@ -60,15 +60,12 @@ extension OnBoardingReactor {
             return signinFailed(message: "알 수 없는 이유로 로그인이 실패했습니다.")
         case .googleSigninCompleted:
             return googleSigninCompleted()
-<<<<<<< HEAD
         case .termsOfServiceButtonDidTap:
             UIApplication.shared.open(URL(string: "https://shy-trust-424.notion.site/f4b4084f6235444bbcc164f7c5d86fb2") ?? .init(string: "https://www.google.com")!)
         case .privacyButtonDidTap:
             UIApplication.shared.open(URL(string: "https://shy-trust-424.notion.site/252fc57341834617b7d3c1903286c730") ?? .init(string: "https://www.google.com")!)
-=======
         case let .appleIdTokenReceived(token, code):
             return appleTokenReceived(idToken: token, code: code)
->>>>>>> 9abd2b24fcafa28513cb0fca7e242dbf11bb396c
         }
         return .empty()
     }
