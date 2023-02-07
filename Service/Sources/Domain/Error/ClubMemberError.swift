f161af25fccea28610e8d98860d67cfd83de11d7
minaje0917 <k050918@naver.com>
2023-02-07 20:56:22 +0900

    :art: 스타일 가이드에 맞게 변경

--- Service/Sources/Domain/Error/ClubMemberError.swift
+++ Service/Sources/Domain/Error/ClubMemberError.swift
@@ -27,20 +27,27 @@ extension ClubMemberError: LocalizedError {
         switch self {
         case let .error(message, _) :
             return message
+            
         case .kickMyself:
             return "자기 자신은 방출할 수 없습니다"
+            
         case .unauthorized, .serverError:
             return "알수없는 에러가 발생했습니다. 잠시 후 다시 시도해 주세요."
+            
         case .delegationMyself:
             return "자기 자신에게 부장을 위임할 수 없습니다"
+            
         // MARK: - 403
         case .notClubHead:
             return "부장이 아니면 사용할 수 없습니다"
+            
         case .notClubMember:
             return "동아리 구성원이 아니면 사용할 수 없습니다"
+            
         // MARK: - 404
         case .notFoundClub:
             return "동아리를 찾을 수 없습니다"
+            
         case .notFoundKickUser:
             return "방출하려는 유저를 찾을 수 없습니다"
         }
