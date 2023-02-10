6b400a4771da1149145f05e8b87891daf8454d9b
minaje0917 <k050918@naver.com>
2023-02-10 22:24:47 +0900

    :fire: remove:: 주석 제거

--- Service/Sources/Domain/UseCases/Applicant/ClubApplyUseCase.swift
+++ Service/Sources/Domain/UseCases/Applicant/ClubApplyUseCase.swift
@@ -1,13 +1,13 @@
-//import RxSwift
-//
-//public struct ClubApplyUseCase {
-//    public init(clubRepository: ClubRepository) {
-//        self.clubRepository = clubRepository
-//    }
-//
-//    private let clubRepository: ClubRepository
-//
-//    public func execute(query: ClubRequestQuery) -> Completable {
-//        clubRepository.apply(query: query)
-//    }
-//}
+import RxSwift
+
+public struct ClubApplyUseCase {
+    public init(clubRepository: ClubRepository) {
+        self.clubRepository = clubRepository
+    }
+
+    private let clubRepository: ClubRepository
+
+    public func execute(query: ClubRequestQuery) -> Completable {
+        clubRepository.apply(query: query)
+    }
+}
