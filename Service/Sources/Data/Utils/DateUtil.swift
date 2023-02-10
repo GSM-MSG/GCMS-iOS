8b5580e0677a544d986d2c183a1b495e8c2d20f8
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-02-10 13:17:10 +0900

    :recycle: 백틱 수정

--- Service/Sources/Data/Utils/DateUtil.swift
+++ Service/Sources/Data/Utils/DateUtil.swift
@@ -3,7 +3,7 @@ import Foundation
 extension String {
     func toDateWithISO8601() -> Date {
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd`T`HH:mm:ss"
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         formatter.locale = Locale(identifier: "Ko_Kr")
         return formatter.date(from: self) ?? .init()
     }
