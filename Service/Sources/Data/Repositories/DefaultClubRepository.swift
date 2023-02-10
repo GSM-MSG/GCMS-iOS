abb36ecda06778e09eed4502b0a6a482ac7fd837
minaje0917 <k050918@naver.com>
2023-02-10 20:47:38 +0900

    :art: API 정상화

--- Service/Sources/Data/Repositories/DefaultClubRepository.swift
+++ Service/Sources/Data/Repositories/DefaultClubRepository.swift
@@ -11,37 +11,25 @@ final class DefaultClubRepository: ClubRepository {
             .doOnNeedRefresh { self.clubLocal.deleteClubList(); self.clubLocal.saveClubList(clubList: $0) }
             .createObservable()
     }
-    func fetchDetailClub(query: ClubRequestQuery) -> Single<Club> {
-        clubRemote.fetchDetailClub(query: query)
+    func fetchDetailClub(clubID: String) -> Single<Club> {
+        clubRemote.fetchDetailClub(clubID: clubID)
     }
     func createNewClub(req: NewClubRequest) -> Completable {
         clubRemote.createNewClub(req: req)
     }
-    func updateClub(req: UpdateClubRequest) -> Completable {
-        clubRemote.updateClub(req: req)
+    func updateClub(clubID: String, req: UpdateClubRequest) -> Completable {
+        clubRemote.updateClub(clubID: clubID, req: req)
     }
-    func deleteClub(query: ClubRequestQuery) -> Completable {
-        clubRemote.deleteClub(query: query)
+    func deleteClub(clubID: String) -> Completable {
+        clubRemote.deleteClub(clubID: clubID)
     }
-    func fetchClubApplicant(query: ClubRequestQuery) -> Single<[User]> {
-        clubRemote.fetchClubApplicant(query: query)
+    func clubOpen(clubID: String) -> Completable {
+        clubRemote.clubOpen(clubID: clubID)
     }
-    func userAccept(query: ClubRequestQuery, userId: String) -> Completable {
-        clubRemote.userAccept(query: query, userId: userId)
+    func clubClose(clubID: String) -> Completable {
+        clubRemote.clubClose(clubID: clubID)
     }
-    func userReject(query: ClubRequestQuery, userId: String) -> Completable {
-        clubRemote.userReject(query: query, userId: userId)
-    }
-    func clubOpen(query: ClubRequestQuery) -> Completable {
-        clubRemote.clubOpen(query: query)
-    }
-    func clubClose(query: ClubRequestQuery) -> Completable {
-        clubRemote.clubClose(query: query)
-    }
-    func apply(query: ClubRequestQuery) -> Completable {
-        clubRemote.apply(query: query)
-    }
-    func cancel(query: ClubRequestQuery) -> Completable {
-        clubRemote.cancel(query: query)
+    func exitClub(clubID: String) -> Completable {
+        clubRemote.exitClub(clubID: clubID)
     }
 }
--- Service/Sources/Domain/Repositories/ClubRepository.swift
+++ Service/Sources/Domain/Repositories/ClubRepository.swift
@@ -2,15 +2,11 @@ import RxSwift
 
 public protocol ClubRepository {
     func fetchClubList(type: ClubType) -> Observable<[ClubList]>
-    func fetchDetailClub(query: ClubRequestQuery) -> Single<Club>
+    func fetchDetailClub(clubID: String) -> Single<Club>
     func createNewClub(req: NewClubRequest) -> Completable
-    func updateClub(req: UpdateClubRequest) -> Completable
-    func deleteClub(query: ClubRequestQuery) -> Completable
-    func fetchClubApplicant(query: ClubRequestQuery) -> Single<[User]>
-    func userAccept(query: ClubRequestQuery, userId: String) -> Completable
-    func userReject(query: ClubRequestQuery, userId: String) -> Completable
-    func clubOpen(query: ClubRequestQuery) -> Completable
-    func clubClose(query: ClubRequestQuery) -> Completable
-    func apply(query: ClubRequestQuery) -> Completable
-    func cancel(query: ClubRequestQuery) -> Completable
+    func updateClub(clubID: String, req: UpdateClubRequest) -> Completable
+    func deleteClub(clubID: String) -> Completable
+    func clubOpen(clubID: String) -> Completable
+    func clubClose(clubID: String) -> Completable
+    func exitClub(clubID: String) -> Completable
 }
