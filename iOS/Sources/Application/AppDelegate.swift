3e11cc5ed6473d6944bb0f75215268a9e02d5c24
baegteun <baegteun@gmail.com>
2023-03-09 15:16:49 +0900

    :sparkles: :: Push 알람 기능 지원

--- iOS/Sources/Application/AppDelegate.swift
+++ iOS/Sources/Application/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         FirebaseApp.configure()
-//        setFCM(application)
+        setFCM(application)
         assembler = Assembler([
             VCAssembly(),
             ReactorAssembly(),
@@ -37,31 +37,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    }
-
 }
 
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
         Messaging.messaging().apnsToken = deviceToken
     }
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
-        if #available(iOS 14.0, *) {
-            return [.badge, .sound, .list, .banner]
-        } else {
-            return [.alert, .badge, .sound]
-        }
-    }
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
-        return
+
+    func messaging(
+        _ messaging: Messaging,
+        didReceiveRegistrationToken fcmToken: String?
+    ) {
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
     }
 }
 
 private extension AppDelegate {
     func setFCM(_ application: UIApplication) {
         UNUserNotificationCenter.current().delegate = self
+        Messaging.messaging().delegate = self
         let authOption: UNAuthorizationOptions = [.alert, .badge, .sound]
         UNUserNotificationCenter.current().requestAuthorization(options: authOption, completionHandler: { _, _ in })
         application.registerForRemoteNotifications()
