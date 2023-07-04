807a2e81a85d7ed21f26e7514e9c6b30df7a2415
junseopark0331 <junseopark0331@icloud.com>
2023-07-04 10:13:20 +0900

    kingfisher 버전을 7.6.2로 올렸습니다

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -11,7 +11,7 @@ let dependencies = Dependencies(
             .remote(url: "https://github.com/RxSwiftCommunity/RxFlow.git", requirement: .exact("2.13.0")),
             .remote(url: "https://github.com/ReactorKit/ReactorKit.git", requirement: .exact("3.2.0")),
             .remote(url: "https://github.com/devxoul/Then.git", requirement: .exact("3.0.0")),
-            .remote(url: "https://github.com/onevcat/Kingfisher.git", requirement: .exact("7.5.0")),
+            .remote(url: "https://github.com/onevcat/Kingfisher.git", requirement: .exact("7.6.2")),
             .remote(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", requirement: .exact("5.0.2")),
             .remote(url: "https://github.com/AliSoftware/Reusable.git", requirement: .exact("4.1.2")),
             .remote(url: "https://github.com/baekteun/BTImageView.git", requirement: .exact("1.1.2")),
