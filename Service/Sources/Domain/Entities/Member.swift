791ce3723ab6e202f215816bf1169a9d573c2ee5
kimkyumbi <kimgyeombi@naver.com>
2024-03-22 12:06:16 +0900

    ♻️ :: 가독성 높이기

--- Service/Sources/Domain/Entities/Member.swift
+++ Service/Sources/Domain/Entities/Member.swift
@@ -1,7 +1,16 @@
 import UIKit
 
 public struct Member: Equatable {
-    public init(uuid: UUID, email: String, name: String, grade: Int, classNum: Int, number: Int, scope: MemberScope, profileImg: String?) {
+    public init(
+        uuid: UUID,
+        email: String,
+        name: String,
+        grade: Int,
+        classNum: Int,
+        number: Int,
+        scope: MemberScope,
+        profileImg: String?
+    ) {
         self.uuid = uuid
         self.email = email
         self.name = name
