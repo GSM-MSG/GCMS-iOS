9f93bcf8cb523babb9d4e9b9223992a085aab8cb
kimkyumbi <kimgyeombi@naver.com>
2024-03-28 23:54:10 +0900

    ✨ :: [#199] ClubAttendRemote 추가

--- Service/Sources/Data/DataSource/Remote/DataSource/ClubAttendRemote.swift
+++ Service/Sources/Data/DataSource/Remote/DataSource/ClubAttendRemote.swift
@@ -1,2 +1,41 @@
 import RxSwift
 import Foundation
+
+protocol ClubAttendRemoteProtocol {
+    func fetchAttendList(clubID: Int, date: String?, period: Period?) -> Single<[ClubAttend]>
+    func attendanceCreate(clubID: Int) -> Single<[ClubAttend]>
+    func recordeExcelPrint() -> Completable
+    func changeAttendStatus() -> Completable
+    func statusAllApply() -> Completable
+}
+
+final class ClubAttendRemote: BaseRemote<ClubAttendAPI>, ClubAttendRemoteProtocol {
+    func fetchAttendList(clubID: Int, date: String?, period: Period?) -> Single<[ClubAttend]> {
+        self.request(.fetchAttendList(clubID: clubID, date: date, period: period))
+            .map(FetchClubAttendListResponse.self)
+            .map { $0.toDomain() }
+    }
+    
+    func attendanceCreate(clubID: Int) -> Single<[ClubAttend]> {
+        self.request(.attendanceCreate(clubID: clubID))
+            .map(FetchClubAttendListResponse.self)
+            .map { $0.toDomain() }
+    }
+    
+    func recordeExcelPrint() -> Completable{
+        self.request(.recordeExcelPrint)
+            .asCompletable()
+    }
+    
+    
+    func changeAttendStatus() -> Completable {
+        self.request(.changeAttendStatus)
+            .asCompletable()
+    }
+    
+    func statusAllApply() -> Completable {
+        self.request(.statusAllApply)
+            .asCompletable()
+    }
+    
+}
