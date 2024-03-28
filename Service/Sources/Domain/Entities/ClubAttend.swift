64b15de47856500a54467800f1ceb8e0455bcd27
kimkyumbi <kimgyeombi@naver.com>
2024-03-29 00:02:52 +0900

    ✨ :: [#199] ClubAttendEntity 추가

--- Service/Sources/Domain/Entities/ClubAttend.swift
+++ Service/Sources/Domain/Entities/ClubAttend.swift
@@ -1,8 +1,29 @@
-//
-//  ClubAttend.swift
-//  Service
-//
-//  Created by Mac on 3/26/24.
-//
-
 import Foundation
+
+public struct ClubAttend: Equatable {
+    public let uuid: UUID
+    public let attendanceID: String
+    public let name: String
+    public let grade: Int
+    public let classNum: Int
+    public let number: Int
+    public let attendanceStatus: AttendanceStatus
+    
+    public init(
+        uuid: UUID,
+        attendanceID: String,
+        name: String,
+        grade: Int,
+        classNum: Int,
+        number: Int,
+        attendanceStatus: AttendanceStatus
+    ) {
+        self.uuid = uuid
+        self.attendanceID = attendanceID
+        self.name = name
+        self.grade = grade
+        self.classNum = classNum
+        self.number = number
+        self.attendanceStatus = attendanceStatus
+    }
+}
