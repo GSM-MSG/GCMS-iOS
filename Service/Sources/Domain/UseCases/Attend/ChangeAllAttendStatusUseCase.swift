9e89cf1ab28ed5336c1b28b4e2f0de0db8160175
kimkyumbi <kimgyeombi@naver.com>
2024-04-15 16:00:53 +0900

    ♻️ :: [#207] UseCase 수정

--- Service/Sources/Domain/UseCases/Attend/ChangeAllAttendStatusUseCase.swift
+++ Service/Sources/Domain/UseCases/Attend/ChangeAllAttendStatusUseCase.swift
@@ -5,9 +5,10 @@ public struct ChangeAllAttendStatusUseCase {
     public init(clubAttendRepository: ClubAttendRepository) {
         self.clubAttendRepository = clubAttendRepository
     }
-
+    
     private let clubAttendRepository: ClubAttendRepository
-
+    
     public func execute(attendanceIds: [String], attendanceStatus: AttendanceStatus) -> Completable {
         clubAttendRepository.changeAllAttendStatus(attendanceIds: attendanceIds, attendanceStatus: attendanceStatus)
     }
+}
--- Service/Sources/Domain/UseCases/Attend/ChangeAttendStatusUseCase.swift
+++ Service/Sources/Domain/UseCases/Attend/ChangeAttendStatusUseCase.swift
@@ -1,14 +1,14 @@
 import RxSwift
 import Foundation
 
-public struct ChangeAllAttendStatusUseCase {
+public struct ChangeAttendStatusUseCase {
     public init(clubAttendRepository: ClubAttendRepository) {
         self.clubAttendRepository = clubAttendRepository
     }
 
     private let clubAttendRepository: ClubAttendRepository
 
-    public func execute(attendanceID: String, attendanceStatus: AttendanceStatus) -> Completable {
-        clubAttendRepository.changeAllAttendStatus(attendanceID: attendanceID, attendanceStatus: attendanceStatus)
+    public func execute(attendanceId: String, attendanceStatus: AttendanceStatus) -> Completable {
+        clubAttendRepository.changeAttendStatus(attendanceId: attendanceId, attendanceStatus: attendanceStatus)
     }
 }
